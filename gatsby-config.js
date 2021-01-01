4f54a28b317d257ecb549c45d06ac97d4d7c8f4a
JoJo <ejolu@live.com>
2021-01-01 13:27:40 -0500

    cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,18 @@ module.exports = {
     {
       resolve: `gatsby-theme-garden`,
       options: {
-        contentPath: `${__dirname}/content/garden`,
-        rootNote: `/hello`,
+        contentPath: `${__dirname}/foam`,
+        rootNote: `/0INDEX`,
+		ignore: [
+          "**/_layouts/**",
+          "**/.git/**",
+          "**/.github/**",
+          "**/.vscode/**"
+		  ]
       },
     },
   ],
   siteMetadata: {
-    title: `Site title`,
+    title: `mimo`,
   },
 }
